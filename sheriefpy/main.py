39b377d7ba70a9dba97069badd7a7d9348165ef9
sherief-ibunsali <104266314+sherief-ibunsali@users.noreply.github.com>
2022-09-14 19:35:10 -0700

    py-doc
    ...

---
+++ sheriefpy/main.py
@@ -0,0 +1,31 @@
+maths=91
+if(maths>30):
+    print("Pass")
+    if (maths > 90):
+        print("Distinction")
+else:
+    print("Fail")
+
+tamil=28
+if(tamil>30):
+    print("Pass")
+else:
+    print("Fail")
+
+english=40
+if(english>30):
+    print("Pass")
+else:
+    print("Fail")
+
+social=89
+if(social>30):
+     print("Pass")
+else:
+     print("Fail")
+
+science=78
+if science>30:
+    print("Pass")
+else:
+    print("Fail")
